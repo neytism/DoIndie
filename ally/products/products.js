2bb0a81bcc1dd90bd35ab52848fa9280b4ad805a
Allysa Jay L. Hipolito <alilyfad26@gmail.com>
2024-10-07 14:44:00 +0800

    added all products gallery

--- ally/products/products.js
+++ ally/products/products.js
@@ -1,11 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper(".swiper", {
-        loop: true,
-        slidesPerView: 5,
-        spaceBetween: 20,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+const swiper = new Swiper('.swiper', {
+
+    loop: true, 
+    spaceBetween: 10, 
+    slidesPerView: 5, 
+    centeredSlides: false, 
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev', 
+    },
+
+    // Responsive 
+    breakpoints: {
+        640: {
+            slidesPerView: 2, 
+            spaceBetween: 20,
         },
-    });
+        768: {
+            slidesPerView: 3, 
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        },
+    },
 });
