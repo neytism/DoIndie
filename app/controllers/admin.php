abc31e4587f96909fd6d9bf32367a52735c7b1f8
CIITSHSDTP00\CIIT <nateodnerolf@gmail.com>
2024-10-18 20:33:39 +0800

    - added admin front end

---
+++ app/controllers/admin.php
@@ -0,0 +1,42 @@
+<?php
+
+class Admin extends Controller
+{
+    private $userModel;
+    
+    public function __construct() {
+        $this->userModel = $this->model('UserModel');
+    }
+    
+    public function index()
+    {
+        $this->view('admin/index');
+        //$this->checkIfLoggedIn();   
+    }
+    
+    public function checkIfLoggedIn(){
+        session_start();
+        
+        $user_info = $this->userModel->checkIfLoggedIn();
+
+        if ($user_info) {
+            $this->view('homeView', ['user_info' => $user_info]);
+        } else {
+            $this->view('homeView');
+        }
+    
+    }
+
+    public function aboutUs()
+    {
+        session_start();
+        
+        $user_info = $this->userModel->checkIfLoggedIn();
+        
+        if ($user_info) {
+            $this->view('aboutUsView', ['user_info' => $user_info]);
+        } else {
+            $this->view('aboutUsView');
+        }
+    }
+}
